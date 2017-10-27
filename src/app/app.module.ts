7f9c3763e278ce9a5e871b34476b9404328f2c36
Shahzad Hussain <shahzad@canbrand.in>
2017-10-27 12:37:31 +0530

    added code for separate routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { routingComponent } from './app-routing.module';
+
+import { LoginService } from './login/login.service';
+import { RegisterService } from './register/register.service';
+import { HomeService } from './home/home.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
-  providers: [],
+  providers: [routingComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
